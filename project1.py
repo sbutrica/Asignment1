2f240019d47f34fec8c0183a9b084cdf8672e2fd
sean butrica <sean.butrica@yahoo.com>
2020-05-31 15:50:00 -0400

    Changed code still don't know if its right.

--- project1.py
+++ project1.py
@@ -1,18 +1,49 @@
 import requests
 
+from bs4 import BeautifulSoup
+
+URL = 'https://www.monster.com/jobs/search/?q=Software-Developer&where=Boston'
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, 'html.parser')
+results = soup.find(id='ResultsContainer')
+job_elems = results.find_all('section', class_='card-content')
+for job_elem in job_elems:
+    title_elem = job_elem.find('h2', class_='title')
+    company_elem = job_elem.find('div', class_='company')
+    location_elem = job_elem.find('div', class_='location')
+    if None in (title_elem, company_elem, location_elem):
+        continue
+
+userInput = input('Please print input keyword')
+userInput_jobs = results.find_all('h2',
+                                  string=lambda text: userInput in text.lower())
+
+print(len(userInput_jobs))
+for p_job in userInput_jobs:
+    link = p_job.find('a')['href']
+    print(p_job.text.strip())
+    print(f"Click here to apply: {link}\n")
+    text_file = open("JobInfo.txt", "w+")
+    text_file.write("Job Posted: %s\n" % p_job.text.strip())
+
+text_file.close()
 
 
-def display_data(to_display):
+def title_elem(userInput):
     pass
 
 
-def get_data():
+def company_elem():
     pass
+
 
 def main():
-    data = get_data()
-    display_data(data)
+    userInput = company_elem()
+    title_elem(userInput)
 
 
 if __name__ == '__main__':
-    main()
+    print("Executed when invoked ")
+else:
+    print("Did not execute")
+
