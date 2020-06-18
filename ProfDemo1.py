7b6e1e682aa90b39a168e6efe98e69f86b28fa72
johns <jsantore@bridgew.edu>
2020-06-18 17:05:10 -0400

    Added a version to show you how I accessed the API data

--- ProfDemo1.py
+++ ProfDemo1.py
@@ -1,8 +1,9 @@
 import requests
 
-#todo put these someplace safer
+# todo put these someplace safer
 API_ID = '<fill yours in here>'
 API_KEY = '<fill yours in here>'
+
 
 def show_data(data):
     for item in data:
@@ -10,12 +11,13 @@ def show_data(data):
             corp = item['company']
             print(corp['display_name'])
 
+
 def get_data(loc:str):
     raw_data = requests.get(loc)
     if raw_data.status_code == 200:
         data = raw_data.json()
         return data['results']
-    else: #something went wrong
+    else:  # something went wrong
         return dict()
 
 
